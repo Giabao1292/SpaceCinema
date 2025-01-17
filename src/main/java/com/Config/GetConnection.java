252a87891e88fc3029baf534e5999ca51b2652c8
Giabao1292 <giabao362004@gmail.com>
2025-01-17 17:04:19 +0700

    Space cinema start

---
+++ src/main/java/com/Config/GetConnection.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lebao
+ */
+public class GetConnection {
+
+    static final String URL = "jdbc:mysql://localhost:3306/PRJ";
+    static final String USER = "root";
+    static final String PASS = "123456";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(URL, USER, PASS);
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Error connection to mysql");
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(GetConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/Repository/impl/SnackRepositoryImpl.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Repository.impl;
+
+/**
+ *
+ * @author lebao
+ */
+public class SnackRepositoryImpl {
+    
+}
